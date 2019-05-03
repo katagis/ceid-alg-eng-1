22f0395d94123b34f3bd1180898ae5e160847ab8
katagis <fox.dealos@gmail.com>
2019-05-03 18:06:20 +0300

    Add checking functions and remove unused code

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 CXX = g++   # use the g++ compiler
 
+VERB_LEVEL=0
+
 LEDA_PATH = /usr/local/LEDA# directory where LEDA libraries are stored
 
 CPPFILE = main.cpp
@@ -7,16 +9,16 @@ CPPFILE = main.cpp
 INCL_LEDA = -I$(LEDA_PATH)/incl
 LINK_LEDAPATH = -L$(LEDA_PATH)
 LINK_LEDA = -lleda
-
+DEF_VERBOSITY = -DVERBOSITY=$(VERB_LEVEL)
 
 LEDA_ALL = $(INCL_LEDA) $(LINK_LEDAPATH) $(LINK_LEDA)
 
 all: run
 
 release:
-	$(CXX) $(CPPFILE) -o release.out $(LEDA_ALL) -O2
+	$(CXX) $(CPPFILE) -o release.out $(DEF_VERBOSITY) $(LEDA_ALL) -O2
 debug:
-	$(CXX) $(CPPFILE) -o debug.out $(LEDA_ALL) -g
+	$(CXX) $(CPPFILE) -o debug.out $(DEF_VERBOSITY) $(LEDA_ALL) -g
 
 run: release
 	./release.out
--- main.cpp
+++ main.cpp
@@ -3,10 +3,15 @@
 
 #include <iostream>
 #include <queue>
+#include <set>
 
 
 #ifdef _MSC_VER
 #define MODERN
+#endif
+
+#ifndef VERBOSITY
+#define VERBOSITY 2
 #endif
 
 using leda::node;
@@ -14,8 +19,11 @@ using leda::graph;
 using leda::edge;
 using leda::list;
 using leda::node_array;
+using leda::edge_array;
 
 #define INT_INF 999999
+
+
 
 
 
@@ -27,20 +35,40 @@ using leda::node_array;
 // Return: All visited nodes
 //
 
-void GraphDebug(const graph& Graph);
+void GraphPrint(const graph& Graph);
 
 std::ostream& operator<< (std::ostream& Stream, const node& Node) {
 	Stream << Node->id();
 	return Stream;
 }
 
-list<node> MyBFS(const graph& Graph, node Start, node_array<int>& OutDist, node_array<edge>& OutPred) {
+std::ostream& operator<< (std::ostream& Stream, const edge& Edge) {
+	Stream << Edge->terminal(0) << "->" << Edge->terminal(1);
+	return Stream;
+}
+
+std::ostream& operator<< (std::ostream& Stream, list<node>& List) {
+	node Node;
+	forall(Node, List) {
+		std::cout << Node << ", ";
+	}
+	return Stream;
+}
+
+enum Color {
+	CNone,
+	CGreen,
+	CBlue
+};
+
+list<node> MyBFS(const graph& Graph, node Start, node_array<int>& OutDist, node_array<edge>& OutPred, edge_array<Color>& OutColorArray) {
 	list<node> Result;
 	
     std::queue<node> Queue;
 
 	OutDist.init(Graph, INT_INF);
 	OutPred.init(Graph);
+	OutColorArray.init(Graph, CNone);
 
     Queue.push(Start);
 	Result.push_back(Start);
@@ -55,13 +83,22 @@ list<node> MyBFS(const graph& Graph, node Start, node_array<int>& OutDist, node_
         node Front = Queue.front();
         Queue.pop();
 		CurrentDistance = OutDist[Front] + 1;
-		
+
+
 		edge Edge;
-		forall_out_edges(Edge, Front) {
+		
+		forall_inout_edges(Edge, Front) {
 			node Node = Graph.target(Edge);
+			if (Node == Front) {
+				Node = Graph.source(Edge);
+			}
+
 			if (OutDist[Node] == INT_INF) {
 				OutDist[Node] = CurrentDistance;
 				OutPred[Node] = Edge; 
+
+				OutColorArray[Edge] = CurrentDistance % 2 ? CGreen : CBlue;
+
 				Result.push_back(Node);
 				Queue.push(Node);
 			}
@@ -71,86 +108,176 @@ list<node> MyBFS(const graph& Graph, node Start, node_array<int>& OutDist, node_
 	return Result;
 }
 
-void Connect(graph& Graph, const std::vector<node>& Nodes, int NodeIndex1, int NodeIndex2) {
-	Graph.new_edge(Nodes[NodeIndex1], Nodes[NodeIndex2]);
-	Graph.new_edge(Nodes[NodeIndex2], Nodes[NodeIndex1]);
-}
+// Assumes List 
+bool MyIsBipartite(const graph& Graph, list<node>& PartA, list<node>& PartB) {
+	list<node> Result;
 
+	node_array<Color> NodeColors;
+	NodeColors.init(Graph, CNone);
 
+	std::queue<node> Queue;
+	node FirstNode = Graph.first_node();
 
-#ifdef MODERN
-template<typename PRED>
-void ForEachNode(const graph& Graph, PRED pred) {
+	Color CurrentColor = CBlue;
+	NodeColors[FirstNode] = CurrentColor;
+	Queue.push(FirstNode);
+	
+	while (!Queue.empty()) {
+		node Front = Queue.front();
+		Queue.pop();
+
+		CurrentColor = NodeColors[Front] == CBlue ? CGreen : CBlue;
+
+		node Node;
+		forall_adj_nodes(Node, Front) {
+			if (NodeColors[Node] == CNone) {
+				NodeColors[Node] = CurrentColor;
+				Queue.push(Node);
+			}
+			else if (NodeColors[Node] != CurrentColor) {
+				return false;
+			}
+		}
+	}
+
+	// Assume no non-connected nodes. 
+	// Perform a different loop to reduce cache misses when accessing PartA and PartB
 	node Node;
 	forall_nodes(Node, Graph) {
-		pred(Node);
+		if (NodeColors[Node] == CBlue) {
+			PartA.push_back(Node);
+		}
+		else {
+			PartB.push_back(Node);
+		}
 	}
+
+
+	return true;
 }
+
+bool IsListSame(const list<node>& ListA, const list<node>& ListB) {
+	std::set<node> SetA;
+	
+	node Node;
+	forall(Node, ListA) {
+		SetA.insert(Node);
+	}
+
+	forall(Node, ListB) {
+		if (SetA.count(Node) == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool TestGraph(const graph& Graph) {
+	if (Graph.empty()) {
+		return true;
+	}
+
+	list<node> MyA, MyB;
+	list<node> LedaA, LedaB;
+
+	bool MyResult = MyIsBipartite(Graph, MyA, MyB);
+	bool LedaResult = leda::Is_Bipartite(Graph, LedaA, LedaB);
+
+	const bool MatchingSize = MyA.size() == LedaA.size() && MyB.size() == LedaB.size();
+
+	const bool TestResult = MatchingSize && IsListSame(MyA, LedaA) && IsListSame(MyB, LedaB);
+
+#if VERBOSITY == 2
+	std::cout << "Ret\t> My: " << MyResult << " | Leda: " << LedaResult << "\n";
+	std::cout << "Size A\t> My: " << MyA.size() << " | Leda: " << LedaA.size() << "\n";
+	std::cout << "Size B\t> My: " << MyB.size() << " | Leda: " << LedaB.size() << "\n";
+
+
+	std::cout << "MyA  : " << MyA << "\n";
+	std::cout << "LedaA: " << LedaA << "\n";
+
+	std::cout << "MyB  : " << MyB << "\n";
+	std::cout << "LedaB: " << LedaB << "\n";
 #endif
-
-bool TestBFS() {
-	graph Graph;
-	std::vector<node> Nodes;
-
-	for (int i = 0; i < 9; ++i) {
-		node Node = Graph.new_node();
-		Nodes.push_back(Node);
+#if VERBOSITY >= 1
+	std::cout << "\nTEST RESULT: ";
+	if (TestResult){
+		std::cout << "PASS\n\n";
 	}
-
-	Connect(Graph, Nodes, 0, 2);
-	Connect(Graph, Nodes, 1, 2);
-	Connect(Graph, Nodes, 2, 3);
-	Connect(Graph, Nodes, 3, 4);
-	Connect(Graph, Nodes, 3, 5);
-	Connect(Graph, Nodes, 5, 6);
-	Connect(Graph, Nodes, 7, 8);
-
-
-	node_array<int> Distance;
-	node_array<edge> VisitPred;
-	list<node> ret = MyBFS(Graph, Nodes[0], Distance, VisitPred);
-
-	if (ret.size() != 7) {
-		return false;
+	else {
+		std::cout << "FAIL\n\n";
 	}
-
-	return Distance[Nodes[6]] == 4
-		&& Distance[Nodes[7]] == INT_INF
-		&& Distance[Nodes[0]] == 0;
+#endif
+	return TestResult;
 }
+
+void GenerateTestGraphs(graph* Graphs, int Count);
 
 int main() {
 
-	if (!TestBFS()) {
-		std::cerr << "MyBFS is broken.\n";
-		return 0;
+	graph Graphs[10];
+
+	GenerateTestGraphs(Graphs, 10);
+
+	bool PassedTests = true;
+	for (int i = 0; i < 10; ++i) {
+		PassedTests &= TestGraph(Graphs[i]);
 	}
 
-	graph Graph;
-	std::vector<node> Nodes;
+	if (PassedTests) {
+		std::cout << "All tests PASSED.\n";
+	}
+	else {
+		std::cout << "Atleast one test failed.\n";
+	}
 	
-	for (int i = 0; i < 9; ++i) {
-		node Node = Graph.new_node();
-		Nodes.push_back(Node);
-	}
-
-	Connect(Graph, Nodes, 0, 2);
-	Connect(Graph, Nodes, 1, 2);
-	Connect(Graph, Nodes, 2, 3);
-	Connect(Graph, Nodes, 3, 4);
-	Connect(Graph, Nodes, 3, 5);
-	Connect(Graph, Nodes, 5, 6);
-	Connect(Graph, Nodes, 7, 8);
-
-
-	node_array<int> Distance;
-	node_array<edge> VisitPred;
-	auto ret = MyBFS(Graph, Nodes[0], Distance, VisitPred);
 
 	getchar();
 }
 
-void GraphDebug(const graph& Graph) {
+void Connect(graph& Graph, const std::vector<node>& Nodes, int NodeIndex1, int NodeIndex2) {
+	Graph.new_edge(Nodes[NodeIndex1], Nodes[NodeIndex2]);
+	Graph.new_edge(Nodes[NodeIndex2], Nodes[NodeIndex1]);
+
+}
+
+
+void Gen_DebugGraph(graph& Graph) {
+	std::vector<node> Nodes;
+
+	for (int i = 0; i < 7; ++i) {
+		node Node = Graph.new_node();
+		Nodes.push_back(Node);
+	}
+
+	Connect(Graph, Nodes, 0, 2);
+	Connect(Graph, Nodes, 1, 2);
+	Connect(Graph, Nodes, 2, 3);
+	Connect(Graph, Nodes, 3, 4);
+	Connect(Graph, Nodes, 3, 5);
+	Connect(Graph, Nodes, 5, 6);
+}
+
+void GenerateTestGraphs(graph* Graphs, int Count) {
+
+	switch (Count) {
+	default:
+
+		// Fallthrough
+	case 2:
+		Gen_DebugGraph(Graphs[1]);
+
+		// Fallthrough
+	case 1:
+		Gen_DebugGraph(Graphs[0]);
+
+		// Fallthrough
+	case 0:
+		;
+	}
+}
+
+void GraphPrint(const graph& Graph) {
 	int i = 0;
 
 	edge e;
